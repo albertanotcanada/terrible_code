26e25c5f6bb02a99db0d6c0cd83f88ea90490603
Alberta Devor <16397283+albertanotcanada@users.noreply.github.com>
2025-04-12 14:10:39 -0400

    Revert "Update main.cpp"

--- main.cpp
+++ main.cpp
@@ -1,4 +1,10 @@
-
+#include <list>
+#include <bitset>
+#include <functional>
+#include "myheader.h"
+#include "utils/math_utils.h"
+#include "config/settings.h"
+#include "logger/logger.h"
 
 int main() {
     std::string name = "Alice";
